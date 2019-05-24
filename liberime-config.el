9fd1b5f2aa282c7e29d3d1b6e9d37393051ae471
A.I <merrick@luois.me>
2019-05-24 16:05:13 +0800

    add simple package header

--- liberime-config.el
+++ liberime-config.el
@@ -1,3 +1,17 @@
+;;; liberime-config.el --- setup liberime automatically
+
+;; Author: A.I.
+;; Keywords: input method, rime
+;; Package-Requires: ((emacs "24.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+
+;; load liberime by default
+
+;;; Code:
+
 (require 'nadvice)
 
 (defvar liberime-shared-data-dir nil)
