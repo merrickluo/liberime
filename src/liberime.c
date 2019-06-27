0ff2eea05ea7542c28166bdde6d2035e15fd8abe
A.I <merrick@luois.me>
2019-06-27 17:40:12 +0800

    fix use user config

--- src/liberime.c
+++ src/liberime.c
@@ -416,7 +416,7 @@ static emacs_value get_config(emacs_env* env, ptrdiff_t nargs, emacs_value args[
   }
 
   RimeConfig *config = malloc(sizeof(RimeConfig));
-  rime->api->config_open(config_id, config);
+  rime->api->user_config_open(config_id, config);
 
   bool success = false;
   emacs_value result;
@@ -439,6 +439,7 @@ static emacs_value get_config(emacs_env* env, ptrdiff_t nargs, emacs_value args[
     result = env->make_string(env, string, strnlen(string, CONFIG_MAXSTRLEN));
   }
 
+  rime->api->config_close(config);
   if (!success) {
     em_signal_rimeerr(env, 2, "failed to get config");
     return em_nil;
@@ -467,7 +468,7 @@ static emacs_value set_config(emacs_env* env, ptrdiff_t nargs, emacs_value args[
   }
 
   RimeConfig *config = malloc(sizeof(RimeConfig));
-  rime->api->config_open(config_id, config);
+  rime->api->user_config_open(config_id, config);
 
   if (strcmp("int", config_type) == 0) {
     int number = env->extract_integer(env, value);
@@ -483,6 +484,7 @@ static emacs_value set_config(emacs_env* env, ptrdiff_t nargs, emacs_value args[
     rime->api->config_set_string(config, config_key, string);
   }
 
+  rime->api->config_close(config);
   return em_t;
 }
 
