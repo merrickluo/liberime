2130baba8e5e15922650bc01de95a1d6c6a1c1f7
Feng Shu <tumashu@163.com>
2026-04-27 11:03:46 +0800

    Do not use when-let*, for ci fail on emacs 26-28

--- liberime.el
+++ liberime.el
@@ -162,29 +162,33 @@ if NAMES is nil, \"rime-data\" as fallback."
 (defun liberime-open-user-data-dir ()
   "Open user data dir with external app."
   (interactive)
-  (when-let* ((user-dir (liberime-get-user-data-dir)))
-    (liberime-open-directory user-dir)))
+  (let ((user-dir (liberime-get-user-data-dir)))
+    (when user-dir
+      (liberime-open-directory user-dir))))
 
 ;;;###autoload
 (defun liberime-open-shared-data-dir ()
   "Open shared data dir with external app."
   (interactive)
-  (when-let* ((shared-dir (liberime-get-shared-data-dir)))
-    (liberime-open-directory shared-dir)))
+  (let ((shared-dir (liberime-get-shared-data-dir)))
+    (when shared-dir
+      (liberime-open-directory shared-dir))))
 
 ;;;###autoload
 (defun liberime-open-package-directory ()
   "Open liberime library directory with external app."
   (interactive)
-  (when-let* ((library-dir (liberime-get-library-directory)))
-    (liberime-open-directory library-dir)))
+  (let ((library-dir (liberime-get-library-directory)))
+    (when library-dir
+      (liberime-open-directory library-dir))))
 
 ;;;###autoload
 (defun liberime-open-package-readme ()
   "Open liberime library README.org."
   (interactive)
-  (when-let* ((library-dir (liberime-get-library-directory)))
-    (find-file (concat library-dir "README.org"))))
+  (let ((library-dir (liberime-get-library-directory)))
+    (when library-dir
+      (find-file (concat library-dir "README.org")))))
 
 ;;;###autoload
 (defun liberime-build ()
