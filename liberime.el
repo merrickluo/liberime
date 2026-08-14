24b0f2e9f535af305767f56a417532b8165eaea8
jixiuf <jixiuf@qq.com>
2026-08-14 18:35:44 +0800

    version 0.0.11

--- liberime.el
+++ liberime.el
@@ -2,7 +2,7 @@
 
 ;; Author: A.I.
 ;; URL: https://github.com/merrickluo/liberime
-;; Version: 0.0.7
+;; Version: 0.0.11
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: convenience, Chinese, input-method, rime
 
